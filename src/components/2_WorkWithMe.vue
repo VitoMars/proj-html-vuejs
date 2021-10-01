a0578bfb78313f4842506c70c2aea5b79d9e6b3b
Vito <vito95v@gmail.com>
2021-10-01 11:35:40 +0200

    Fine creazione cerchi

--- src/components/2_WorkWithMe.vue
+++ src/components/2_WorkWithMe.vue
@@ -10,12 +10,44 @@
       commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
       velit esse cillum dolore eu fugiat nulla.
     </p>
-    <!-- Circle -->
-    <div class="box">
-      <svg>
-        <circle cx="100" cy="100" r="100"></circle>
-      </svg>
-      <div class="icon"></div>
+    <!-- Circles -->
+    <div class="circles">
+      <!-- Box 1 -->
+      <div class="box">
+        <div class="relative">
+          <svg>
+            <circle cx="100" cy="100" r="100"></circle>
+          </svg>
+          <div class="icon">
+            <i class="fas fa-drafting-compass"></i>
+          </div>
+          <h5>WEB DESIGN</h5>
+        </div>
+      </div>
+      <!-- Box2 -->
+      <div class="box">
+        <div class="relative">
+          <svg>
+            <circle cx="100" cy="100" r="100"></circle>
+          </svg>
+          <div class="icon">
+            <i class="fas fa-shield-alt"></i>
+          </div>
+          <h5>WEB BRANDING</h5>
+        </div>
+      </div>
+      <!-- Box 3 -->
+      <div class="box">
+        <div class="relative">
+          <svg>
+            <circle cx="100" cy="100" r="100"></circle>
+          </svg>
+          <div class="icon">
+            <i class="far fa-gem"></i>
+          </div>
+          <h5>UI / UX</h5>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -28,36 +60,6 @@ export default {
 
 <style scoped lang="scss">
 @import "./style/colors.scss";
-
-// Test 1
-.box {
-  position: relative;
-  width: 300px;
-  height: 400px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-
-  svg {
-    position: relative;
-    width: 220px;
-    height: 220px;
-    transform: rotate(-90deg);
-    circle {
-      width: 220px;
-      height: 220px;
-      fill: none;
-      stroke-width: 10;
-      stroke: $radicalRed;
-      transform: translate(5px, 5px);
-      stroke-dasharray: calc(100 * 6);
-      stroke-dashoffset: calc((100 * 6) - ((100 * 6) * 95) / 100);
-      stroke-linecap: round;
-    }
-  }
-}
-
 .workWithMe {
   display: flex;
   flex-direction: column;
@@ -72,8 +74,76 @@ h6 {
   color: $text-secondary;
 }
 
+h5 {
+  font-size: 14px;
+  letter-spacing: 4px;
+  font-weight: inherit;
+  margin-top: 50px;
+}
+
 p {
   max-width: 900px;
+  color: $text-primary;
+}
+
+// Cerchi
+.circles {
+  width: 70vw;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 50px;
+  .box {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    .relative {
+      position: relative;
+      width: 210px;
+      height: 210px;
+      svg {
+        position: relative;
+        max-width: 210px;
+        height: 210px;
+        transform: rotate(-90deg);
+        circle {
+          width: 200px;
+          height: 200px;
+          fill: none;
+          stroke-width: 10px;
+          stroke: $radicalRed;
+          transform: translate(5px, 5px);
+          stroke-dasharray: calc(100 * 6);
+          stroke-linecap: round;
+        }
+      }
+    }
+  }
+}
+
+.circles .box:nth-child(1) svg circle {
+  stroke-dashoffset: calc((100 * 6) - ((100 * 6) * 92) / 100);
+}
+
+.circles .box:nth-child(2) svg circle {
+  stroke-dashoffset: calc((100 * 6) - ((100 * 6) * 85) / 100);
+}
+
+.circles .box:nth-child(3) svg circle {
+  stroke-dashoffset: calc((100 * 6) - ((100 * 6) * 99) / 100);
+}
+
+.icon {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 60px;
   color: $text-primary;
 }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,5 @@
-<template
-  ><div>
+<template>
+  <div class="main">
     <AboutMe />
     <WorkWithMe />
   </div>
